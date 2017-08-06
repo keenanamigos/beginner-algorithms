a1475ba564cdef4d337046628c7d6c36ec7b79d7
Keenan Johnson <keenan.johnson1@outlook.com>
2017-08-05 19:14:14 -0500

    Add tests for backwards iteration of the array

--- test/arrayIterator_test.js
+++ test/arrayIterator_test.js
@@ -37,3 +37,41 @@ describe('Forward iteration', () => {
         expect(iterator.isDone()).to.be.true;
     });
 });
+
+describe('Backwards Iteration', () => {
+    const someArray = ["A", "B", "C"];
+    const ArrayIterator = require('../src/Iterators/ArrayIterator').ArrayIterator;
+    let iterator = new ArrayIterator(someArray);
+
+    iterator.last();
+    it('should return false', () => {
+        expect(iterator.isDone()).to.be.false;
+    });
+
+    it('should be equal to the last element in the array', () => {
+        expect(someArray[2]).to.equal(iterator.current());
+    }); 
+    
+    it('should return false', () => {
+        iterator.previous();
+        expect(iterator.isDone()).to.be.false;
+    });
+
+    it('shoud be equal to the second element in the array', () => {
+        expect(someArray[1]).to.equal(iterator.current());
+    });
+
+    it('should return false', () => {
+        iterator.previous();
+        expect(iterator.isDone()).to.be.false;
+    });
+
+    it('should be equal to the first element in the array', () => {
+        expect(someArray[0]).to.equal(iterator.current());
+    });
+
+    it('should return true', () => {
+        iterator.previous();
+        expect(iterator.isDone()).to.be.true;
+    });
+});
